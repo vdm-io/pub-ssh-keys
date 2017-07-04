fdeb8b05ad5d89e5ed4fb81d80766255ca1cb978
Llewellyn van der Merwe <llewellyn@vdm.io>
2017-07-04 15:53:47 +0100

    oops missed some vars... :)

--- vdmkeys.sh
+++ vdmkeys.sh
@@ -10,11 +10,11 @@ else
 fi
 
 # Variables
-RACKUSER="vdm"
-RACKHOME="/home/vdm"
-RACKSCRIPT="https://raw.githubusercontent.com/vdm-io/pub-ssh-keys/$BRANCH/vdmkeys.sh"
-RACKKEYS="https://raw.githubusercontent.com/vdm-io/pub-ssh-keys/$BRANCH/authorized_keys"
-RACKCHECKSUM="https://raw.githubusercontent.com/vdm-io/pub-ssh-keys/$BRANCH/authorized_keys.md5sum"
+VDMUSER="vdm"
+VDMHOME="/home/vdm"
+VDMSCRIPT="https://raw.githubusercontent.com/vdm-io/pub-ssh-keys/$BRANCH/vdmkeys.sh"
+VDMKEYS="https://raw.githubusercontent.com/vdm-io/pub-ssh-keys/$BRANCH/authorized_keys"
+VDMCHECKSUM="https://raw.githubusercontent.com/vdm-io/pub-ssh-keys/$BRANCH/authorized_keys.md5sum"
 
 # Require script to be run via sudo, but not as root
 if [[ $EUID -ne 0 ]]; then
@@ -23,35 +23,35 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Add and configure vdm user access
-if getent passwd $RACKUSER > /dev/null; then
+if getent passwd $VDMUSER > /dev/null; then
 	echo "VastDevelopmentMethod Management User already exists...Skipping"
 else
 	echo -n "Adding the VastDevelopmentMethod Management User..."
-	useradd -m -d $RACKHOME $RACKUSER
+	useradd -m -d $VDMHOME $VDMUSER
 	echo "Done"
 fi
 
 echo -n "Checking file checksum..."
-mkdir -p $RACKHOME/.ssh
-curl -s -o $RACKHOME/.ssh/authorized_keys.md5sum $RACKCHECKSUM
-curl -s -o $RACKHOME/.ssh/authorized_keys $RACKKEYS
-(cd $RACKHOME/.ssh && md5sum -c authorized_keys.md5sum)
+mkdir -p $VDMHOME/.ssh
+curl -s -o $VDMHOME/.ssh/authorized_keys.md5sum $VDMCHECKSUM
+curl -s -o $VDMHOME/.ssh/authorized_keys $VDMKEYS
+(cd $VDMHOME/.ssh && md5sum -c authorized_keys.md5sum)
 
 echo -n "Correcting SSH configuration permissions..."
-chmod 600 $RACKHOME/.ssh/authorized_keys
-chmod 500 $RACKHOME/.ssh
-chown -R $RACKUSER:$RACKUSER $RACKHOME/.ssh
+chmod 600 $VDMHOME/.ssh/authorized_keys
+chmod 500 $VDMHOME/.ssh
+chown -R $VDMUSER:$VDMUSER $VDMHOME/.ssh
 echo "Done"
 
-if [ -f $RACKHOME/vdm.cron ]; then
+if [ -f $VDMHOME/vdm.cron ]; then
 	echo "Crontab already configured for updates...Skipping"
 else
 	echo -n "Adding crontab entry for continued updates..."
-	echo "MAILTO=\"\"" > $RACKHOME/vdm.cron
-	echo "" >> $RACKHOME/vdm.cron
-	echo "@reboot curl -s $RACKSCRIPT | sudo bash" >> $RACKHOME/vdm.cron
-	echo "*/15 * * * * curl -s $RACKSCRIPT | sudo bash" >> $RACKHOME/vdm.cron
-	crontab -u $RACKUSER $RACKHOME/vdm.cron
+	echo "MAILTO=\"\"" > $VDMHOME/vdm.cron
+	echo "" >> $VDMHOME/vdm.cron
+	echo "@reboot curl -s $VDMSCRIPT | sudo bash" >> $VDMHOME/vdm.cron
+	echo "*/15 * * * * curl -s $VDMSCRIPT | sudo bash" >> $VDMHOME/vdm.cron
+	crontab -u $VDMUSER $VDMHOME/vdm.cron
 	echo "Done"
 fi
 
@@ -60,8 +60,8 @@ if [ -f /etc/sudoers.d/vdm-user ]; then
 else
 	echo -n "Configuring sudo for VastDevelopmentMethod Management User..."
 	echo "# VastDevelopmentMethod user allowed sudo access" > /etc/sudoers.d/vdm-user
-	echo "$RACKUSER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/vdm-user
-	echo "Defaults:$RACKUSER !requiretty" >> /etc/sudoers.d/vdm-user
+	echo "$VDMUSER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/vdm-user
+	echo "Defaults:$VDMUSER !requiretty" >> /etc/sudoers.d/vdm-user
 	echo "" >> /etc/sudoers.d/vdm-user
 	chmod 440 /etc/sudoers.d/vdm-user
 	echo "Done"
